1a32fe07245b09446813d98de7961657d0bb65c7
Francisco Sebastian Licudis <141773688+fliquid23@users.noreply.github.com>
2025-08-24 17:19:03 -0300

    chore: scaffold monorepo

---
+++ mobile/lib/core/theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+final lightTheme = ThemeData(useMaterial3: true, brightness: Brightness.light);
+final darkTheme  = ThemeData(useMaterial3: true, brightness: Brightness.dark);
